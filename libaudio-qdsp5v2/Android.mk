07ebd410e4410b039efd8b0d946b1086c80bc265
Mihai Moldovan <ionic@ionic.de>
2011-11-25 20:25:02 +0100

    Merged with upstream
    Change-Id: I9730ad90edc72b441b8698897fd37194da83f77b

--- libaudio-qdsp5v2/Android.mk
+++ libaudio-qdsp5v2/Android.mk
@@ -40,10 +40,10 @@ LOCAL_SRC_FILES += AudioHardware.cpp
 
 LOCAL_CFLAGS += -fno-short-enums
 
-LOCAL_STATIC_LIBRARIES += libaudiointerface
-ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-  LOCAL_SHARED_LIBRARIES += liba2dp
-endif
+#LOCAL_STATIC_LIBRARIES += libaudiointerface
+#ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+#  LOCAL_SHARED_LIBRARIES += liba2dp
+#endif
 
 include $(BUILD_SHARED_LIBRARY)
 
