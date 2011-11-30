21ecf8e062fb558e1e17b25ec00f86f1529c170b
Twisted Playground <twistedumbrella@gmail.com>
2011-11-30 17:56:12 -0500

    Merge Updates
    Replace temp fix with modified version of newly added msm7k Android.mk.
    Remove faulty checks and revise definitions.

--- Android.mk
+++ Android.mk
@@ -19,18 +19,37 @@ ifeq ($(BOARD_USES_QCOM_LIBRPC),true)
     LIBRPC := librpc-qcom
 endif
 
-common_msm_dirs := libcopybit liblights libopencorehw $(LIBRPC) libstagefrighthw
-msm7k_dirs := $(common_msm_dirs) boot libgralloc libaudio
-qsd8k_dirs := $(common_msm_dirs) libgralloc-qsd8k libaudio-qsd8k dspcrashd
-msm7x30_dirs := liblights libgralloc-qsd8k $(LIBRPC) libaudio-msm7x30
+common_msm_dirs := liblights $(LIBRPC) libstagefrighthw
+msm7k_dirs := $(common_msm_dirs) boot libaudio libcopybit dspcrashd
+qsd8k_dirs := $(common_msm_dirs) libaudio-qsd8k dspcrashd libcopybit
+msm7x30_dirs := $(common_msm_dirs) libaudio-msm7x30 liboverlay libcopybit libsensors
 
-ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
+#For Dragon Board APQ8060, ALSA ADUIO is used for WOLFSON CODEC
+ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
+  msm8660_dirs := $(common_msm_dirs) dspcrashd
 else
-   ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
-     include $(call all-named-subdir-makefiles,$(qsd8k_dirs))
-   else
-     ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
-       include $(call all-named-subdir-makefiles,$(msm7x30_dirs))
-     endif
-   endif
- endif
+  msm8660_dirs := $(common_msm_dirs) libaudio-msm8660 dspcrashd
+endif
+msm7x27a_dirs := $(common_msm_dirs) boot libaudio-msm7x27a dspcrashd
+msm8960_dirs := $(common_msm_dirs) dspcrashd
+
+ifeq ($(call is-chipset-in-board-platform,msm7x30),true)
+  include $(call all-named-subdir-makefiles,$(msm7x30_dirs))
+else
+  ifeq ($(call is-board-platform,msm7627a),true)
+    include $(call all-named-subdir-makefiles,$(msm7x27a_dirs))
+  else
+    include $(call all-named-subdir-makefiles,$(msm7k_dirs))
+  endif
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+  include $(call all-named-subdir-makefiles,$(qsd8k_dirs))
+else
+  ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+    include $(call all-named-subdir-makefiles,$(msm8660_dirs))
+  else
+    ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+      include $(call all-named-subdir-makefiles,$(msm8960_dirs))
+    endif
+  endif
+endif
