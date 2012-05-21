525541bf8698ca053b8a11cea122d08ab9e9cce2
Nicholas Flintham <nick@flinny.org>
2012-05-21 07:22:13 +0100

    not needed

--- Android.mk
+++ Android.mk
@@ -18,7 +18,7 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
 ifneq ($(BOARD_USES_QCOM_LEGACY),true)
 
-    common_msm_dirs := liblights libstagefrighthw
+    common_msm_dirs := libstagefrighthw
     qsd8k_dirs := $(common_msm_dirs) dspcrashd libcopybit
     msm7x30_dirs := $(common_msm_dirs) liboverlay
 
